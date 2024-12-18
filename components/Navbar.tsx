dd77acd8e8aa602904ba569c293d7201320300f1
Anmol <cakely63@gmail.com>
2024-12-18 18:11:27 +0530

    SEO fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex items-center w-full relative z-30 py-5 bg-transparent text-white">
-      <Link href="https://oxumlabs.github.io/">
+      <Link href="/">
         <Image src="/logo.svg" alt="logo" width={74} height={29} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex justify-center items-center m-auto">
